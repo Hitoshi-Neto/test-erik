54eb2cd31d257f3c3e3ea554fd9a95ef3b154f59
João Palmeira <palmeiraishiineto@gmail.com>
2025-03-30 14:23:36 -0300

    arquivo-chat

---
+++ script.js
@@ -0,0 +1,7 @@
+// Efeito de digitação automática
+var typed = new Typed(".auto-type", {
+    strings: ["um desenvolvedor", "um designer", "um criador de ideias"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
